6c7957e1c7939161349e9e225623d21ae50b6e1c
Hussein Ahmed <59267542+Husseinbnahmed@users.noreply.github.com>
2023-03-02 22:42:59 -0500

    wide layout config

--- Airbnb_streamlit.py
+++ Airbnb_streamlit.py
@@ -4,6 +4,8 @@ import pandas as pd
 import Airbnb as bnb
 import streamlit as st
 
+st.set_page_config(layout="wide")
+
 # set the title of the application
 st.title("Short Term Rental Evaluation App 🏠")
 
